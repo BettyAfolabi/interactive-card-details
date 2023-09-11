ec11d886eb0c76159b117d37d7fa2dc26206c3a7
Elizabeth <betty4web@gmail.com>
2023-09-11 02:48:45 +0100

    styling desktop view

--- src/App.jsx
+++ src/App.jsx
@@ -3,91 +3,138 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="bg-[url('./bg-main-desktop.png')] bg-no-repeat bg-screen h-screen">
-        <div className="pt-7">
-          <div className="w-[18rem] h-[10rem] m-3 relative">
-            <img src="./bg-card-back.png" alt="card back" />
-            <p className="absolute top-16 right-8 text-white text-sm">000</p>
+      <div className="flex font-SpaceGrotesk tracking-wider">
+        <div className="relative w-3/6">
+          <img
+            src="./bg-main-desktop.png"
+            alt="background image"
+            className="h-screen"
+          />
+          <div className="absolute top-1/2 left-1/2">
+            <div className="w-80 relative mt-4">
+              <img src="./bg-card-back.png" alt="card back" />
+              <p className="absolute top-20 right-8 text-white text-sm">000</p>
+            </div>
           </div>
-          <div className="w-[18rem] h-[10rem] relative mb-3">
-            <img src="./bg-card-front.png" className=" m-3" />
-            <img src="./card-logo.svg" className="absolute top-5 left-7 w-16" />
-            <h1 className="absolute top-1/2 left-6 text-white text-2xl min-w-full ">
-              0000 0000 0000 0000
+          <div className="absolute top-1/4 left-1/3">
+            <div className="relative mb-3 w-80">
+              <img src="./bg-card-front.png" />
+              <img
+                src="./card-logo.svg"
+                className="absolute top-5 left-5 w-16"
+              />
+              <h1 className="absolute top-1/2 left-6 text-white text-2xl min-w-full ">
+                0000 0000 0000 0000
+              </h1>
+              <p className="absolute bottom-4 left-5 text-white">Jane Doe</p>
+              <h3 className="absolute bottom-4 right-5 text-white">00/00</h3>
+            </div>
+          </div>
+        </div>
+        <div className="bg-white p-5 mb-3 h-screen grid place-items-center w-full">
+          <form className="w-64 h-64 ">
+            <div className="my-5">
+              <label htmlFor="cardname" className="text-xs text-DarkerViolet ">
+                CARDHOLDER NAME
+              </label>
+              <input
+                type="text"
+                id="cardname"
+                name="cardname"
+                placeholder="e.g Jane Doe"
+                className="block rounded-lg border-solid border-Grey border px-3 py-2 w-80 mt-1"
+              />
+            </div>
+            <div className="my-5">
+              <label
+                htmlFor="cardnumber"
+                className="text-xs text-DarkerViolet "
+              >
+                CARD NUMBER
+              </label>
+              <input
+                type="number"
+                id="cardnumber"
+                name="cardnumber"
+                placeholder="e.g 0000 0000 0000 0000"
+                className="block rounded-lg border-solid border-Grey border px-3 py-2 w-80 mt-1"
+              />
+            </div>
+            <div className="flex my-5">
+              <div>
+                <label
+                  htmlFor="expirydate"
+                  className="text-xs text-DarkerViolet "
+                >
+                  EXP. DATE (MM/YY)
+                </label>
+                <div className="flex ">
+                  <input
+                    type="text"
+                    id="month"
+                    name="month"
+                    placeholder="MM"
+                    className="block rounded-lg border-solid border-Grey border px-3 py-2 w-16 mr-2 mt-1"
+                  />
+                  <input
+                    type="text"
+                    id="year"
+                    name="year"
+                    placeholder="YY"
+                    className="block rounded-lg border-solid border-Grey border px-3 py-2 w-16 mt-1"
+                  />
+                </div>
+              </div>
+              <div className="ml-6">
+                <label htmlFor="cvc" className="text-xs text-DarkerViolet ">
+                  CVC
+                </label>
+                <input
+                  type="number"
+                  id="cvc"
+                  name="cvc"
+                  placeholder="e.g 123"
+                  className="block rounded-lg border-solid border-Grey border px-3 py-2 w-40 mt-1"
+                />
+              </div>
+            </div>
+            <button className="bg-DarkerViolet text-white w-80 rounded-md p-2 text-base my-3">
+              Confirm
+            </button>
+          </form>
+          <div className="p-5 text-center hidden">
+            <img
+              src="./icon-complete.svg"
+              alt="complete icon"
+              className="mx-auto w-16 mb-7"
+            />
+            <h1 className="text-DarkerViolet text-2xl mb-2 tracking-widest">
+              THANK YOU!
             </h1>
-            <div className=" text-white">
-              <p className="absolute bottom-3 left-5">Jane Doe</p>
-              <h3 className="absolute bottom-3 right-5">00/00</h3>
-            </div>
+            <h3 className="text-DarkGrayViolet tracking-normal mb-2">
+              We`ve added your card details
+            </h3>
+            <button className="bg-DarkerViolet text-white w-80 rounded-md p-2 text-base mt-7">
+              Continue
+            </button>
           </div>
         </div>
-        <form className="bg-white p-5 mb-3 h-screen">
-          <div>
-            <label htmlFor="cardname">CARDHOLDER NAME</label>
-            <input
-              type="text"
-              id="cardname"
-              name="cardname"
-              className="block rounded-lg border-solid border-Grey border p-1"
-            />
-          </div>
-          <div>
-            <label htmlFor="cardnumber">CARD NUMBER</label>
-            <input
-              type="number"
-              id="cardnumber"
-              name="cardnumber"
-              className="block rounded-lg border-solid border-Grey border p-1"
-            />
-          </div>
-          <div>
-            <label htmlFor="expirydate">EXPIRY DATE (MM/YY)</label>
-            <div className="flex ">
-              <input
-                type="text"
-                id="month"
-                name="month"
-                placeholder="MM"
-                className="block rounded-lg border-solid border-Grey border p-1 w-16 mr-2"
-              />
-              <input
-                type="text"
-                id="year"
-                name="year"
-                placeholder="YYYY"
-                className="block rounded-lg border-solid border-Grey border p-1 w-16 ml-3"
-              />
-            </div>
-          </div>
-          <div>
-            <label htmlFor="cvc">CVC</label>
-            <input
-              type="number"
-              id="cvc"
-              name="cvc"
-              className="block rounded-lg border-solid border-Grey border p-1"
-            />
-          </div>
-        </form>
-        <div className="attribution bottom-0 text-xs text-center lg:mt-4 lg:text-White">
-          Challenge by
-          <a
-            href="https://www.frontendmentor.io?ref=challenge"
-            target="_blank"
-            className="text-attribution"
-            rel="noreferrer"
-          >
-            Frontend Mentor
-          </a>
-          . Coded by
-          <a
-            href="https://github.com/BettyAfolabi"
-            className="text-attribution"
-          >
-            Betty
-          </a>
-          .
-        </div>
+      </div>
+      <div className="attribution bottom-0 text-xs text-center lg:mt-4 lg:text-White">
+        Challenge by
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          className="text-attribution"
+          rel="noreferrer"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by
+        <a href="https://github.com/BettyAfolabi" className="text-attribution">
+          Betty
+        </a>
+        .
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        Red: "hsl(0, 100%, 66%)",
+        Violet: "hsl(249, 99%, 64%)",
+        LightGrayViolet: "hsl(270, 3%, 87%)",
+        DarkGrayViolet: "hsl(279, 6%, 55%)",
+        DarkerViolet: " hsl(278, 68%, 11%)",
+        White: "hsl(0, 0%, 100%)",
+        attribution: "hsl(228, 45%, 44%)",
+      },
+      textColor: {
+        DarkerViolet: " hsl(278, 68%, 11%)",
+      },
+      fontFamily: {
+        sans: ["Helvetica", "Arial", "sans-serif"],
+        SpaceGrotesk: ["Space Grotesk", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
